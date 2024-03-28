7a5549dea99e0f313d0b2536b34d1fdff9d203e8
Akram <matakram872@gmail.com>
2024-03-28 21:32:19 +0000

    first_commit

---
+++ Projet.java
@@ -0,0 +1,13 @@
+public class Projet{
+    class Immobilier{
+        String type, localisation, description;
+        int taille, prix;
+
+    }
+    class Client{
+        String name;
+    }
+    public static void main() {
+        System.out.print("hello");
+    }
+}
